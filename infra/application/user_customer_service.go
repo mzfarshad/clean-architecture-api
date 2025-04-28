8fef556c75f0e010685a8578689338e67470d3c4
mzfarshad <farshad.akbari.arzati@gmail.com>
2025-04-28 17:15:41 +0330

    feat: implement customer use case in  user_customer_service.go

--- infra/application/user_customer_service.go
+++ infra/application/user_customer_service.go
@@ -1,1 +1,29 @@
 package application
+
+import (
+	"context"
+
+	"github.com/mzfarshad/music_store_api/infra/domain/user"
+)
+
+func NewCustomerService(userRepo user.Repository) user.CustomerUseCase {
+	return &customerService{
+		userRepo: userRepo,
+	}
+}
+
+type customerService struct {
+	userRepo user.Repository
+}
+
+func (s *customerService) UpdateMyName(ctx context.Context, name, email string) error {
+	usr, err := s.userRepo.FirstByEmail(ctx, email)
+	if err != nil {
+		return err
+	}
+	usr.Name = name
+	if err := s.userRepo.Update(ctx, usr); err != nil {
+		return err
+	}
+	return nil
+}
