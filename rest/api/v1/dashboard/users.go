d56a1a7c4b6dc449f913299bacdf1ca570e2bd5a
mzfarshad <farshad.akbari.arzati@gmail.com>
2025-05-06 22:34:42 +0330

    implement admin use case in dashboard

--- rest/api/v1/dashboard/users.go
+++ rest/api/v1/dashboard/users.go
@@ -8,20 +8,102 @@ import (
 
 func usersRouter(v1Dashboard fiber.Router, userService user.AdminUseCase) {
 	// TODO
-	//users := v1Dashboard.Group("/users")
-	//users.Get("", searchInUsers())
-	//users.Get("/:id", getUser())
-	//users.Post("", createNewUser())
-	//users.Put("/:id", updateUser())
-	//users.Put("/:id/deactivate", deactivateUser())
-	//users.Put("/:id/reactivate", reactivateUser())
+	users := v1Dashboard.Group("/users")
+	users.Get("", searchInUsers(userService))
+	users.Put("/updateMyProfile", updateMyProfile(userService))
+	users.Put("/deactivate/:id", deactivateUser(userService))
+	users.Put("/reactivate/:id", reactivateUser(userService))
 }
 
-type getUser struct {
+type deactiveUser struct {
 	rest.DTO `json:"-"`
-	Id       uint `params:"id" validate:"required"` // TODO: Test this
+	Id       uint   `params:"id" validate:"required"` // TODO: Test this
+	Reason   string `json:"reason" validate:"required"`
 }
 
-// func getUser() {
-// rest.Request[getUser]{}.
-//}
+func deactivateUser(userService user.AdminUseCase) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		input, err := rest.Request[deactiveUser]{}.Parse(ctx)
+		if err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		if err := userService.DeactivateUser(ctx.Context(), input.Id, input.Reason); err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		return nil
+	}
+}
+
+type reactiveUser struct {
+	rest.DTO `json:"_"`
+	Id       uint `params:"id" validate:"required"`
+}
+
+func reactivateUser(userService user.AdminUseCase) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		input, err := rest.Request[reactiveUser]{}.Parse(ctx)
+		if err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		if err := userService.ReactivateUser(ctx.Context(), input.Id); err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		return nil
+	}
+}
+
+type updateProfile struct {
+	rest.DTO `json:"_"`
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required"`
+}
+
+func updateMyProfile(userService user.AdminUseCase) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		input, err := rest.Request[updateProfile]{}.Parse(ctx)
+		if err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		if err := userService.UpdateMyProfile(ctx.Context(), input.Name, input.Email); err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		return nil
+	}
+}
+
+type searchUsers struct {
+	rest.DTO `json:"_"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Limit    int    `json:"limit"`
+	Page     int    `json:"page"`
+}
+
+type pagination struct {
+	rest.DTO       `json:"_"`
+	userPagination user.PaginationParams
+}
+
+func searchInUsers(userService user.AdminUseCase) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		input, err := rest.Request[searchUsers]{}.Parse(ctx)
+		if err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		var searchParam user.SearchParams
+		searchParam.Limit = input.Limit
+		searchParam.Name = input.Name
+		searchParam.Email = input.Email
+		searchParam.Page = input.Page
+
+		users, err := userService.SearchInUsers(ctx.Context(), searchParam)
+		if err != nil {
+			return rest.NewFailed(err).Handle(ctx)
+		}
+		var usersPage pagination
+		usersPage.userPagination.TotalPages = users.TotalPages
+		usersPage.userPagination.TotalData = users.TotalData
+		usersPage.userPagination.Result = users.Result
+		return rest.NewSuccess(usersPage).Handle(ctx)
+	}
+}
